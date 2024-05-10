0e67dd1a18ced239f2b48fecd075d680e03561bb
michalrozekariane <michal.rozek@arianelabs.com>
2024-05-10 15:00:09 +0200

    feat: added missing copyright text for some test files
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- src/test/e2e/calculate-rarity-from-on-chain-data-e2e.test.ts
+++ src/test/e2e/calculate-rarity-from-on-chain-data-e2e.test.ts
@@ -1,3 +1,22 @@
+/*-
+ *
+ * Hedera NFT SDK
+ *
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import { LONG_E2E_TIMEOUT, MIRROR_NODE_DELAY } from '../__mocks__/consts';
 import { calculateRarityFromOnChainData } from '../../rarity';
 import { LINK_TO_JSON_OBJECT_WITHOUT_ERRORS, nftSDK, operatorPrivateKey } from './e2e-consts';
